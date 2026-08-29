63d82c697cb56d71fbdba49329b911dcc77c0fe5
nnc <notnoelchannel1@protonmail.com>
2026-08-29 19:42:34 +0200

    fix: net/core/sysctl_net_core.c

--- net/core/sysctl_net_core.c
+++ net/core/sysctl_net_core.c
@@ -138,14 +138,15 @@ static struct ctl_table net_core_table[] = {
 		.maxlen		= sizeof(int),
 		.mode		= 0644,
 		.proc_handler	= proc_dointvec
-	},
 #ifdef CONFIG_BPF_JIT
 	{
 		.procname	= "bpf_jit_enable",
 		.data		= &bpf_jit_enable,
 		.maxlen		= sizeof(int),
 		.mode		= 0644,
-		.proc_handler	= proc_dointvec
+		.proc_handler	= proc_dointvec_minmax,
+		.extra1		= &zero,
+		.extra2		= &one,
 	},
 #endif
 	{
